2cab28a01270aa3186abb68b1669f6fd7b2a5c65
joaquinf <fernandezjoaquinpk@gmail.com>
2024-10-09 14:30:30 -0300

    better optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,32 +1,30 @@
 import cv2
 import os
 
-def ImageCompressor(path, output_format='jpg', quality=75):
+def ImageCompressor(path, output_format='jpg', quality=75, scale=1.0):
     # Verifica que el archivo existe
     if not os.path.isfile(path):
         raise FileNotFoundError(f"No se encontró el archivo: {path}")
 
-    # Obtener la extensión del archivo original
-    _, extension = os.path.splitext(path)
-    
-    # Leer la imagen en escala de grises
+    # Leer la imagen en escala de grises directamente
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     if img is None:
         raise ValueError("No se pudo leer la imagen. Verifique el formato y la integridad del archivo.")
 
-    # Redimensionar la imagen (ajustar si es necesario)
-    N_WIDTH = int(img.shape[1] * 2)  # Mantiene el ancho original
-    N_HEIGHT = int(img.shape[0] * 2)  # Mantiene la altura original
-    IMG_RES = cv2.resize(img, (N_WIDTH, N_HEIGHT), interpolation=cv2.INTER_NEAREST)
+    # Redimensionar la imagen si es necesario
+    if scale != 1.0:
+        N_WIDTH = int(img.shape[1] * scale)
+        N_HEIGHT = int(img.shape[0] * scale)
+        img = cv2.resize(img, (N_WIDTH, N_HEIGHT), interpolation=cv2.INTER_LINEAR)  # INTER_LINEAR es más rápido y suave
 
     # Crear la nueva ruta para la imagen optimizada
     path_without_extension = os.path.splitext(path)[0]
-    new_path = f"{path_without_extension}.{output_format}"
+    new_path = f"{path_without_extension}_compressed.{output_format}"
 
-    # Guardar la imagen en el formato deseado
+    # Guardar la imagen en el formato deseado con compresión ajustada
     if output_format.lower() in ['jpg', 'jpeg']:
-        cv2.imwrite(new_path, IMG_RES, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+        cv2.imwrite(new_path, img, [cv2.IMWRITE_JPEG_QUALITY, quality])
     else:
-        cv2.imwrite(new_path, IMG_RES)  # Para otros formatos, ajustar según sea necesario
+        cv2.imwrite(new_path, img)  # Para otros formatos como PNG no hay calidad de compresión ajustable
 
     return new_path
--- pytessract.py
+++ pytessract.py
@@ -6,8 +6,8 @@ from optimizer import ImageCompressor
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-PATH_TO_IMAGE = "test.webp"
-NEW_PATH = ImageCompressor(PATH_TO_IMAGE)
+PATH_TO_IMAGE = "test.jpg"
+NEW_PATH = ImageCompressor(PATH_TO_IMAGE, 'jpg', 75)
 
 text = pytesseract.image_to_string(NEW_PATH, lang='spa')
 
@@ -17,6 +17,6 @@ print("Tiempo de proceso: ", time.process_time())
 
 img_display = cv2.imread(NEW_PATH)
 
-cv2.imshow("imagen-op", img_display)
+cv2.imshow("imagen-reconocida", img_display)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
